241a8f1c167d66a7f5271a3f6ef522db777d9c38
Jidan Ahmed Alternate <nasimaferdausi@Jidans-MacBook-Air.local>
2026-01-01 04:27:17 -0500

    added windows and basic keyboard input

--- placeholder-name/main.c
+++ placeholder-name/main.c
@@ -1,14 +1,50 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <ncurses.h>
+#include <signal.h>
+void cleanup(int sig) {
+    endwin();
+    exit(0);
+}
 
 int main() {
+  // setup ncurses
+  signal(SIGINT, cleanup);
   initscr();
+  noecho();
+  cbreak();
+  keypad(stdscr, 1);
+  nodelay(stdscr, 1);
+  curs_set(0);
 
-  printw("hello world");
+  // create windows
+  int h, w;
+  getmaxyx(stdscr, h, w);
+  WINDOW *game = newwin(h, 2*w/3, 0, 0);
+  WINDOW *ui = newwin(h, w/3, 0, 2*w/3);
 
-  refresh();
+  int x = 0;
+  int y = 0;
 
-  getch();
+  while (1) {
+    // refresh windows
+    wrefresh(game);
+    wrefresh(ui);
+
+    // box windows
+    box(game, 0, 0);
+    box(ui, 0, 0);
+
+
+    mvwaddch(game,y,2*x,'#');
+
+    int ch = getch();
+    if (ch == 27) break;    // escape key
+    if (ch == KEY_UP) y--;
+    if (ch == KEY_DOWN) y++;
+    if (ch == KEY_LEFT) x--;
+    if (ch == KEY_RIGHT) x++;
+  }
 
   endwin();
 
